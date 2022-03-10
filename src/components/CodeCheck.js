50e09ca1ac1546d02eee3c44c7b02d5b895dd417
ayesareact1 <ayesareact1@gmail.com>
2022-03-10 15:26:31 +0100

    useeffect

--- src/components/CodeCheck.js
+++ src/components/CodeCheck.js
@@ -4,6 +4,7 @@ export default function CodeCheck(props) {
 
     const [codigo, setCodigo] = useState();
     const [isShowInput, setIsShowInput] = useState(true);
+    const [contador, setContador] = useState(20);
 
     const handleChange = e => { // e es el evento con los valores de control de JavaScript
         setCodigo(e.target.value);
@@ -12,15 +13,26 @@ export default function CodeCheck(props) {
     useEffect(() => {
         if (codigo === '1234') {
             setIsShowInput(false);
+            setContador(0);
         }
     }, [codigo])
 
+    useEffect(() => {
+        const timer = setInterval(() => {
+            if (contador > 0) {
+                setContador(prevContador => prevContador - 1)
+            }
+        }, 1000)
+        return () => clearInterval(timer);  // Callback opcional de limpieza
+    }, [contador])
+
   return (
     <>
-        { isShowInput ?
+        { isShowInput ? 
             <>
                 <p>Introduzca el código recibido por SMS</p>
-                <input type='text' value={codigo} onChange={handleChange}/>
+                <input type='text' onChange={handleChange}/>
+                <p className="clock">{contador}</p>
             </>
             :
             <p>Código correcto, bienvenid@</p>
